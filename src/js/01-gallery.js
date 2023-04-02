f1145576e6b73ae95c8130dc90a4f477f60382c3
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-04-02 14:23:43 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import sifmplelightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
+const galleryCard = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+  </a>
+</div>`;
+    })
+        .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryCard);
+
+new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInputData(event) {
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(localStorageKey, JSON.stringify(objectToSave));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+    localStorage.removeItem(localStorageKey);
+}
+
+
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error('Get state error: ', error.message);
+    }
+};
+
+const storageData = load(localStorageKey);
+if (storageData) {
+    email.value = storageData.email;
+    message.value = storageData.message;
+}
